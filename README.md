18ac65e5aff4d7fc825b54dea3ab2743273ea6fa
Eray Demirci <eraydemirciofficial@hotmail.com>
2026-07-06 18:50:40 +0300

    Kodlar düzenlendi. .gitignore ve README.md dosyaları eklendi.

---
+++ README.md
@@ -0,0 +1,44 @@
+# Secret Notes
+
+## EN:
+
+### How to Encrypt Text?
+
+- Enter a title in the "Enter Your Title" section.
+
+- Enter the text you want to encrypt in the "Enter Your Secret" section.
+
+- Enter the password you want to use in the "Enter Your Master Key" section.
+
+- When you click the "Save & Encrypt" button, a file named "mysecret.txt" will be created, and the text you entered 
+will be stored in this file in encrypted form.
+
+### How to Decrypt Encrypted Text?
+
+- Enter the encrypted text from the "mysecret.txt" file in the "Enter Your Secret" section.
+
+- Enter the correct password in the "Enter Your Master Key" section.
+
+- When you click the "Decrypt" button (if your password is correct), the encrypted text will be decrypted.
+
+
+## TR:
+
+### Şifreleme İşlemi Nasıl Yapılır?
+
+- "Enter Your Title" bölümüne bir başlık giriniz.
+
+- "Enter Your Secret" bölümüne şifrelenmesini istediğiniz metni giriniz.
+
+- "Enter Your Master Key" bölümüne kullanılmasını istediğiniz parolayı giriniz.
+
+- "Save & Encrypt" butonuna bastığınızda "mysecret.txt" adında bir dosya oluşacak ve girdiğiniz metin şifrelenmiş 
+olarak bu dosyada tutulacaktır.
+
+### Şifrelenmiş Metin Nasıl Normale Döndürülür?
+
+- "Enter Your Secret" bölümüne "mysecret.txt" dosyasında bulunan şifreli metni yazınız.
+
+- "Enter Your Master Key" bölümüne doğru parolayı giriniz. 
+
+- "Decrypt" butonuna bastığınızda (eğer parolanız doğru ise) şifrelenmiş metin normale dönecektir.
---
+++ main.py
@@ -0,0 +1,81 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import onetimepad
+
+
+# window
+root = Tk()
+root.title("Top Secret")
+root.config(bg="lightgrey")
+root.geometry("500x500")
+
+# icon
+pic = Image.open("topsecret.png")
+pic = pic.resize((100, 100), Image.LANCZOS)
+
+icon = ImageTk.PhotoImage(pic)
+icon_label = Label(root, image=icon, bg="lightgrey")
+icon_label.pack(pady=20)
+
+def encrypting():
+    t1 = secret_text.get("1.0", END).strip()
+    k1 = key_entry.get()
+
+    enc = onetimepad.encrypt(t1, k1)
+
+    if title_entry.get() == "" or key_entry.get() == "" or secret_text.get("1.0", END) == "":
+        messagebox.showwarning("Warning", "Please fill in all fields before encrypting or decrypting.")
+    else:
+        with open("mysecret.txt", "a") as f:
+            f.write("Title: " + title_entry.get() + "\n")
+            f.write("Encrypted Text: " + enc + "\n")
+            f.write("-" * 20 + "\n")
+
+def decrypting():
+    if key_entry.get() == "" or secret_text.get("1.0", END) == "":
+        messagebox.showwarning("Warning", "Please fill in all fields before encrypting or decrypting.")
+
+    t2 = secret_text.get("1.0", END).strip()
+    k2 = key_entry.get()
+
+    dec = onetimepad.decrypt(t2, k2)
+
+    secret_text.delete("1.0", END)
+    secret_text.insert("1.0", dec)
+
+# title label
+title_label = Label(root, text="Enter Your Title", bg="lightgrey", fg="black")
+title_label.pack()
+
+# title entry
+title_entry = Entry(root, bg="white", fg="black", highlightthickness=0, insertbackground="black")
+title_entry.focus()
+title_entry.pack()
+
+# secret label
+secret_label = Label(root, text="Enter Your Secret", bg="lightgrey", fg="black")
+secret_label.pack()
+
+# secret text
+secret_text = Text(root, bg="white", fg="black", insertbackground="black", height=10, width=30, highlightthickness=0, bd=2, relief="sunken")
+secret_text.pack()
+
+# key label
+key_label = Label(root, text="Enter Master Key", bg="lightgrey", fg="black")
+key_label.pack()
+
+# key entry
+key_entry = Entry(root, bg="white", fg="black",insertbackground="black", highlightthickness=0)
+key_entry.pack()
+
+# encrypt button
+encrypt_button = Button(root, text="Save & Encrypt", highlightbackground="lightgrey", relief="flat",
+                        command=encrypting)
+encrypt_button.pack(pady=10)
+
+# decrypt button
+decrypt_button = Button(root, text="Decrypt", highlightbackground="lightgrey", relief="flat", command=decrypting)
+decrypt_button.pack()
+
+root.mainloop()
